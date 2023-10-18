cb084d592559e5e8397556ed99286a59d57e09ac
MICastilloHurtado <122422130+MICastilloHurtado@users.noreply.github.com>
2023-10-18 12:54:33 -0500

    ahora si

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 server.use(express.json())
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://food-frontend-puce.vercel.app'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://food-frontend-puce.vercel.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
